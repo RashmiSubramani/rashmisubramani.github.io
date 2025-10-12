2e2ef19ae94f8438f95851c6f0abfc5efbd1e49a
RashmiSubramani <rashmisubramani15@gmail.com>
2025-10-12 08:31:09 +0530

    refactor: added resume and fixed logo

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash]-' + Date.now() + '.js',
-        chunkFileNames: 'assets/[name]-[hash]-' + Date.now() + '.js',
-        assetFileNames: 'assets/[name]-[hash]-' + Date.now() + '.[ext]'
-      }
-    }
-  }
+  base: '/'
 })
